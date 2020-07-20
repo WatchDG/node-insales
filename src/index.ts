32096c05967191b7d899f8cb433e630880113705
WatchDG <baranov@w3m.co>
2020-07-20 13:10:21 +0300

    new version 2.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import {ResultFail, ResultOk} from 'node-result';
-import Axios, {AxiosInstance} from 'axios';
+import { ResultFail, ResultOk } from 'node-result';
+import Axios, { AxiosInstance } from 'axios';
 
 type DeliveryVariantId = number;
 type DeliveryVariantTitle = string;
@@ -9,84 +9,84 @@ type DeliveryVariantDescription = string;
 type WebHookId = number;
 
 type DeliveryVariant = {
-    delivery_variant: {
-        title: DeliveryVariantTitle;
-        type: DeliveryVariantType;
-        description?: DeliveryVariantDescription;
-    };
+  delivery_variant: {
+    title: DeliveryVariantTitle;
+    type: DeliveryVariantType;
+    description?: DeliveryVariantDescription;
+  };
 };
 
 type PaymentDeliveryVariant = {
-    id: number;
-    delivery_variant_id: number;
-    created_at: string;
+  id: number;
+  delivery_variant_id: number;
+  created_at: string;
 };
 
 type PaymentGatewayType = 'PaymentGateway::Custom';
 
 type PaymentGateway = {
-    id: number;
-    position: number;
-    type: PaymentGatewayType;
-    created_at: string;
-    updated_at: string;
-    margin: string;
-    available_for_individual_clients: boolean;
-    available_for_juridical_clients: boolean;
-    title: string;
-    description: string;
-    payment_delivery_variants: PaymentDeliveryVariant[];
+  id: number;
+  position: number;
+  type: PaymentGatewayType;
+  created_at: string;
+  updated_at: string;
+  margin: string;
+  available_for_individual_clients: boolean;
+  available_for_juridical_clients: boolean;
+  title: string;
+  description: string;
+  payment_delivery_variants: PaymentDeliveryVariant[];
 };
 
 export class InSales {
-    private readonly instance: AxiosInstance;
+  private readonly instance: AxiosInstance;
 
-    constructor(baseURL: string, timeout: number = 1000, headers: object = {}) {
-        this.instance = Axios.create({baseURL, timeout, headers});
-    }
+  constructor(baseURL: string, timeout: number = 1000, headers: object = {}) {
+    this.instance = Axios.create({ baseURL, timeout, headers });
+  }
 
-    async getDeliveryVariants() {
-        try {
-            const {data} = await this.instance.get('/admin/delivery_variants.json');
-            return ResultOk(data);
-        } catch (error) {
-            return ResultFail(error);
-        }
+  async getDeliveryVariants() {
+    try {
+      const { data } = await this.instance.get('/admin/delivery_variants.json');
+      return ResultOk(data);
+    } catch (error) {
+      return ResultFail(error);
     }
+  }
 
-    async getDeliveryVariant(id: DeliveryVariantId) {
-        try {
-            const {data} = await this.instance.get(`/admin/delivery_variants/${id}.json`);
-            return ResultOk(data);
-        } catch (error) {
-            return ResultFail(error);
-        }
+  async getDeliveryVariant(id: DeliveryVariantId) {
+    try {
+      const { data } = await this.instance.get(`/admin/delivery_variants/${id}.json`);
+      return ResultOk(data);
+    } catch (error) {
+      return ResultFail(error);
     }
+  }
 
-    async destroyDeliveryVariant(id: DeliveryVariantId) {
-        try {
-            const {data} = await this.instance.delete(`/admin/delivery_variants/${id}.json`);
-            return ResultOk(data);
-        } catch (error) {
-            return ResultFail(error);
-        }
+  async destroyDeliveryVariant(id: DeliveryVariantId) {
+    try {
+      const { data } = await this.instance.delete(`/admin/delivery_variants/${id}.json`);
+      return ResultOk(data);
+    } catch (error) {
+      return ResultFail(error);
     }
+  }
 
-    async getPaymentGateways() {
-        try {
-            const {data} = await this.instance.get('/admin/payment_gateways.json');
-            return ResultOk(data);
-        } catch (error) {
-            return ResultFail(error);
-        }
+  async getPaymentGateways() {
+    try {
+      const { data } = await this.instance.get('/admin/payment_gateways.json');
+      return ResultOk(data);
+    } catch (error) {
+      return ResultFail(error);
     }
+  }
 
-    async destroyWebHook(id: WebHookId) {
-        try {
-            const {data} = await this.instance.delete(`/admin/webhooks/${id}.json`);
-            return ResultOk(data);
-        } catch (error) {
-            return ResultFail(error);
-        }
+  async destroyWebHook(id: WebHookId) {
+    try {
+      const { data } = await this.instance.delete(`/admin/webhooks/${id}.json`);
+      return ResultOk(data);
+    } catch (error) {
+      return ResultFail(error);
     }
+  }
 }
